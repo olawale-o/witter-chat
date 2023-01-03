3b83c80774c9dea6d983f1737ef0225d113d954d
olawale-o <omoogunolawale@gmail.com>
2023-01-03 02:58:23 +0100

    Fix services url

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,11 +1,11 @@
 import { post } from "../api";
 
 export const loginService = async (body) => {
-  const data = await post('http://localhost:4000/api/v1/users/login', body);
+  const data = await post('http://localhost:5000/api/v1/users/login', body);
   return data;
 };
 
 export const registerService = async (body) => {
-  const data = await post('http://localhost:4000/api/v1/users/register', body);
+  const data = await post('http://localhost:5000/api/v1/users/register', body);
   return data;
 };
--- src/services/friendService.js
+++ src/services/friendService.js
@@ -1,32 +1,33 @@
 import { get, post } from "../api";
 
 export const getUnfollowedUsers = async (username) => {
-  const data = await get(`http://localhost:4000/api/v1/users?q=${username}`);
+  console.log(username);
+  const data = await get(`http://localhost:5000/api/v1/users?q=${username}`);
   return data.users;
 };
 
 export const getPendingFriendRequest = async (userId) => {
-  const data = await get(`http://localhost:4000/api/v1/users/pending?q=${userId}`);
+  const data = await get(`http://localhost:5000/api/v1/users/pending?q=${userId}`);
   return data.friends;
 };
 
 
 export const addFriendRequestService = async (body) => {
-  const data = await post('http://localhost:4000/api/v1/users/friends', body);
+  const data = await post('http://localhost:5000/api/v1/users/friends', body);
   return data;
 };
 
 export const acceptFriendRequestService = async (body) => {
-  const data = await post('http://localhost:4000/api/v1/users/friends/accept', body);
+  const data = await post('http://localhost:5000/api/v1/users/friends/accept', body);
   return data;
 };
 
 export const getFriendSuggestionService = async (query) => {
-  const data = await get(`http://localhost:4000/api/v1/users/suggestion?q=${query}`);
+  const data = await get(`http://localhost:5000/api/v1/users/suggestion?q=${query}`);
   return data.suggestions;
 };
 
 export const getContactService = async (id) => {
-  const data = await get(`http://localhost:4000/api/v1/users/${id}`);
+  const data = await get(`http://localhost:5000/api/v1/users/${id}`);
   return data.user;
 };
