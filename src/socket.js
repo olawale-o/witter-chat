3cb857f37a532f8a7532f37fb30c761bb0f35037
olawale-o <omoogunolawale@gmail.com>
2023-01-03 02:58:04 +0100

    Fix socket url

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,5 @@
 import io from 'socket.io-client';
-const URL = 'http://localhost:4000';
+const URL = 'http://localhost:5000';
 const socket = io(URL, { autoConnect: false });
 
 export default socket;
