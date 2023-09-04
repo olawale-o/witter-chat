c9437297fef7b78c598cfb81d3bb68f71314dec8
olawale-o <omoogunolawale@gmail.com>
2023-09-04 13:57:27 +0100

    Fetch suggested friends

--- src/pages/Friends/Suggestion.js
+++ src/pages/Friends/Suggestion.js
@@ -1,30 +1,55 @@
+import { useState, useMemo } from "react";
 import { useLoaderData } from "react-router-dom";
 import { getFriendSuggestionService } from "../../services/friendService";
+import { useSocketContext } from "../../context/socket";
+
+const userId = JSON.parse(localStorage.getItem('user')).user?._id;
 
 export async function loader() {
-  return getFriendSuggestionService('suggestion')  
+  return getFriendSuggestionService(userId);  
 }
 
 const FriendSuggestion = () => {
-  const followers = useLoaderData();
+  const data = useLoaderData();
+  const { toggleFollow } = useSocketContext();
+  const [followingList, setFollowingList] = useState([]);
+
+  const ids = useMemo(() => followingList, [followingList]);
+  
+  const onToggleFollow = (user,) => {
+    if (ids.includes(user._id)) {
+      toggleFollow(user, userId, 'unfollow')
+      const newList = followingList.filter((id) => id !== user?._id);
+      setFollowingList(newList);
+    } else {
+      toggleFollow(user, userId, 'follow',)
+      setFollowingList((prevState) => ([...prevState, user._id]))
+    }
+  };
   return (
     <ul className="friend-list">
       {
-        followers.users.slice(0, 20).map((follower) => (
-          <li className="friend-item" key={follower._id}>
+        data.users.slice(0, 20).map((user) => (
+          <li className="friend-item" key={user._id}> 
             <div className="friend">
               <div className="left">
                 <div className="friend-item__image-container">
                   {
-                    follower?.avatar ? (<img className="img" src={follower.avatar} alt="avatar" />) : 
-                    (<span>{follower.username[0].toUpperCase()}</span>)
+                    user?.avatar ? (<img className="img" src={user.avatar} alt="avatar" />) : 
+                    (<span>{user.username[0].toUpperCase()}</span>)
                   }
                 </div>
                 <div className="summary">
-                  <span className="title">{follower.username}</span>
+                  <span className="title">{user.username}</span>
                 </div>
               </div>
-              <button type="button" className="cta">Follow</button>
+              <button
+                type="button"
+                className="cta"
+                onClick={() => onToggleFollow(user)}
+              >
+                {ids.includes(user._id) ? 'Unfollow' : 'Follow'}
+              </button>
             </div>
           </li>
         ))
