1a15861f2147ae0f7b6c4eb29344d095d04a038f
olawale-o <omoogunolawale@gmail.com>
2023-10-10 02:55:10 +0100

    Fix socket url

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,5 @@
 import io from 'socket.io-client';
-const URL = process.env.NODE_ENV === 'https://native-chat.adaptable.app' ? undefined : 'http://localhost:5000';
+const URL = process.env.NODE_ENV !== 'development' ? 'https://native-chat.adaptable.app' : 'http://localhost:5000';
 const socket = io(URL, { autoConnect: false });
 
 export default socket;
