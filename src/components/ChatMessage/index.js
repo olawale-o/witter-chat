83b468340a3e07623929cdc75782db1222021c2c
olawale-o <omoogunolawale@gmail.com>
2023-10-17 01:39:01 +0100

    Refactor ChatMessage component

--- src/components/ChatMessage/index.js
+++ src/components/ChatMessage/index.js
@@ -1,5 +1,24 @@
 import './style.css';
 
+
+const Message = ({ message, isFromMe = true}) => {
+  return (
+    <div className={`chat-message-container ${isFromMe ? 'chat-sender': 'chat-recipient'}`}>
+      <div className="avatar-container" />
+      <div className="message-area">
+        <div className="chat-message-content">
+          <span className="chat-profile-name">You</span>
+          <p className="chat-message text-sm">{message.message}</p>
+        </div>
+        <div className="chat-message-timestamp">
+          <span className="timestamp text-sm">16:45</span>
+          <div className="online-state online small-circle" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
 const ChatMessage = ({
   messages,
 }) => {
@@ -8,35 +27,9 @@ const ChatMessage = ({
       {
         messages?.map((message, i) => (
           message.from === JSON.parse(localStorage.getItem('user'))?.user._id ? (
-            <div className="chat-message-container chat-sender" key={i}>
-              <div className="avatar-container" />
-              <div className="message-area">
-                <div className="chat-message-content">
-                  <span className="chat-profile-name">You</span>
-                  <p className="chat-message text-sm">{message.message}</p>
-                </div>
-                <div className="chat-message-timestamp">
-                  <span className="timestamp text-sm">16:45</span>
-                  <div className="online-state online small-circle" />
-                </div>
-              </div>
-            </div>
+            <Message key={i} message={message} isFromMe />
           ) : 
-          (
-            <div className="chat-message-container chat-recipient" key={i}>
-              <div className="avatar-container" />
-                <div className="message-area">
-                  <div className="chat-message-content">
-                    <span className="chat-profile-name">{message.username}</span>
-                    <p className="chat-message text-sm">{message.message}</p>
-                  </div>
-                <div className="chat-message-timestamp">
-                  <span className="timestamp text-sm">16:45</span>
-                  <div className="online-state online small-circle" />
-                </div>
-              </div>
-            </div>
-          )
+          (<Message key={i} message={message} isFromMe={false} />)
         ))
       }
     </div>
