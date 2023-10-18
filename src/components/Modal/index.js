c056a7457a46f1ef4573627240fb142f0d146b1c
olawale-o <omoogunolawale@gmail.com>
2023-10-19 00:25:30 +0100

    Create modal component

---
+++ src/components/Modal/index.js
@@ -0,0 +1,13 @@
+import "./style.css";
+
+const Modal = ({ children }) => {
+  return (
+    <div className="modal">
+      <div className="modal-content">
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export { Modal };
