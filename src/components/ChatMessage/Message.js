8334183193bd6077c0f918fd299c4520d8078e64
olawale-o <omoogunolawale@gmail.com>
2023-10-19 02:58:19 +0100

    Refactor ChatMessage component

---
+++ src/components/ChatMessage/Message.js
@@ -0,0 +1,38 @@
+import Logo from "../../assets/logo.png";
+
+const Text = ({ message }) => <p className="chat-message text-sm">{message}</p>;
+const Image = ({ image }) => (
+  <div className="image-chat">
+    <img src={image.message ?? Logo}  alt="preview" />
+   {image.caption && <p className="text-md">{image.caption}</p>}
+  </div>
+);
+
+const Item = ({ item }) => {
+  switch(item.type) {
+    case 'image':
+      return <Image image={item} />
+    default:
+      return <Text message={item.message} />
+  }
+};
+
+const Message = ({ message, isFromMe = true}) => {
+  return (
+    <div className={`chat-message-container ${isFromMe ? 'chat-sender': 'chat-recipient'}`}>
+      <div className="avatar-container" />
+      <div className="message-area">
+        <div className="chat-message-content">
+          <span className="chat-profile-name">You</span>
+          <Item item={message} />
+        </div>
+        <div className="chat-message-timestamp">
+          <span className="timestamp text-sm">16:45</span>
+          <div className="online-state online small-circle" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export { Message };
--- src/components/ChatMessage/index.js
+++ src/components/ChatMessage/index.js
@@ -1,37 +1,6 @@
 import './style.css';
-import Logo from "../../assets/logo.png";
 
-const Message = ({ message, isFromMe = true}) => {
-  return (
-    <div className={`chat-message-container ${isFromMe ? 'chat-sender': 'chat-recipient'}`}>
-      <div className="avatar-container" />
-      <div className="message-area">
-        <div className="chat-message-content">
-          <span className="chat-profile-name">You</span>
-          {
-            message.type === "text" ? (<p className="chat-message text-sm">{message.message}</p>)
-            : message.type === "image" && message.message === undefined ? (
-              <div className="image-chat">
-                <img src={Logo}  alt="preview" />
-                {message.caption && <p className="text-md">{message.caption}</p>}
-              </div>
-            )
-            : (
-              <div className="image-chat">
-                <img src={message.message}  alt="preview" />
-                {message.caption && <p className="text-md">{message.caption}</p>}
-              </div>
-            )
-          }
-        </div>
-        <div className="chat-message-timestamp">
-          <span className="timestamp text-sm">16:45</span>
-          <div className="online-state online small-circle" />
-        </div>
-      </div>
-    </div>
-  );
-};
+import { Message } from './Message';
 
 const ChatMessage = ({
   messages,
@@ -42,8 +11,7 @@ const ChatMessage = ({
         messages?.map((message, i) => (
           message.from === JSON.parse(localStorage.getItem('user'))?.user._id ? (
             <Message key={i} message={message} isFromMe />
-          ) : 
-          (<Message key={i} message={message} isFromMe={false} />)
+          ) : (<Message key={i} message={message} isFromMe={false} />)
         ))
       }
     </div>
